54fb92a0dbc51bf59cc27fcc954f2355a06631a4
shashwatkashyap28 <shashwatkashyap2005@gmail.com>
2026-06-21 15:45:13 +0530

    initial commit from root

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <header className='bg-slate-200 shadow-md '> 
+    <header className='bg-slate-900 shadow-md '> 
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>GO</span>
-            <span className='text-slate-700'>REALTOR</span>
+            <span className='text-slate-100'>GO</span>
+            <span className='text-green-500'>REALTOR</span>
           </h1>
         </Link>
         <form className='bg-slate-100 p-3 rounded-lg flex'>
@@ -20,13 +20,13 @@ export default function Header() {
         </form>
         <ul className='flex gap-4'>
           <Link to='/'>
-          <li className='hidden sm:inline text-slate-700 hover:underline'>HOME</li>
+          <li className='hidden sm:inline text-slate-100 hover:underline'>HOME</li>
           </Link>
           <Link to='/about'>
-          <li className='hidden sm:inline text-slate-700 hover:underline'>ABOUT</li>
+          <li className='hidden sm:inline text-slate-100 hover:underline'>ABOUT</li>
           </Link>
           <Link to='/signin'>
-          <li className='text-slate-700 hover:underline'>SIGN IN</li>
+          <li className='text-slate-100 hover:underline'>SIGN IN</li>
           </Link>
         </ul>
       </div>  
