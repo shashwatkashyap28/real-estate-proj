76bba70ac8d1222907da01f1e449417a88ffaf7c
shashwatkashyap28 <shashwatkashyap2005@gmail.com>
2026-06-28 16:53:33 +0530

    create a middleware

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,19 +1,26 @@
 import User from "../../models/user.model.js";
 import argon2 from "argon2";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async(req, res) => {
-  const { username, email, password } = req.body;
-  try {
-    const hash = await argon2.hash(password);
-    const newUser = new User({ username, email, password: hash });
-    await newUser.save();
-    res.status(201).json({ message: "User created successfully" });
-  } catch (err) {
-    res.status(500).json({ message: "Error creating user", error: err.message }); 
-    
-  }
-  const newUser = new User({ username, email, password });
-  await newUser.save();
-  res.status(201).json({ message: "User created successfully" });
+export const signup = async (req, res, next) => {
+    const { username, email, password } = req.body;
 
+    try {
+        const hash = await argon2.hash(password);
+
+        const newUser = new User({
+            username,
+            email,
+            password: hash,
+        });
+
+        await newUser.save();
+
+        res.status(201).json({
+            message: "User created successfully",
+        });
+
+    } catch (err) {
+        next(error);
+    }
 };
