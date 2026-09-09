a0db28a1a8e5750cde6a01cd430f89b3305e43f6
shashwatkashyap28 <shashwatkashyap2005@gmail.com>
2026-09-09 23:56:28 +0530

    . \

--- client/src/pages/signin.jsx
+++ client/src/pages/signin.jsx
@@ -7,7 +7,7 @@ import {
   signInFailure,
 } from '../redux/user/userSlice';
 import OAuth from '../components/OAuth';
-import { safeFetchJson } from '../utils/api';
+import { safeFetchJson } from './utils/api';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
