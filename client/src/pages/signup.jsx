14d1d135450f3a5744b1ca338a67c11ce930c62f
shashwatkashyap28 <shashwatkashyap2005@gmail.com>
2026-09-09 23:59:14 +0530

    .

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import OAuth from '../components/OAuth';
-import { safeFetchJson } from '../utils/api';
+import { safeFetchJson } from '../utils/api' ;
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
